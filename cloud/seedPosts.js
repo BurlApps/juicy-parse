5b385bcf129da0eed70f91af5328b5f1178e5166
Brian Vallelunga <vallelungabrian@gmail.com>
2014-08-11 01:59:46 -0700

    update

--- cloud/seedPosts.js
+++ cloud/seedPosts.js
@@ -1,9 +1,17 @@
 Parse.Cloud.define("seedPosts", function(req, res) {
   var Post = Parse.Object.extend("Posts")
   var user = Parse.User.current()
+  var images = [
+    "http://www.heykiki.com/blog/wp-content/uploads/2013/09/a49.jpg",
+    "http://www.wired.com/images_blogs/underwire/2013/01/mf_ddp_large.jpg",
+    "http://cdn.surf.transworld.net/wp-content/blogs.dir/443/files/2013/08/Vans-Party.jpg",
+    "http://norwich.tab.co.uk/files/2012/10/house-party21.jpg",
+    "http://cdn.lipstiq.com/wp-content/uploads/2014/02/cover3.jpg",
+    "http://static6.businessinsider.com/image/51f0432069beddd20a000004/email-ad-exec-demands-free-food-for-90-people-at-a-going-away-party.jpg"
+  ]
 
   Parse.Cloud.httpRequest({
-    url: "http://www.wired.com/images_blogs/underwire/2013/01/mf_ddp_large.jpg"
+    url: images[Math.floor(Math.random() * (images.length - 1))]
   }).then(function(imageData) {
     var image = new Parse.File("background.jpg",  {
       base64: imageData.buffer.toString('base64')
