8fa509344c31e90444f0c02f5cef53d82eb7cca5
Brian Vallelunga <vallelungabrian@gmail.com>
2014-09-22 02:13:43 -0700

    added beta testers selection on new user

--- cloud/beforeSave/users.js
+++ cloud/beforeSave/users.js
@@ -1,36 +1,42 @@
 var Testers = Parse.Object.extend("Testers")
+var Settings = require("cloud/util/settings")
 
 Parse.Cloud.beforeSave(Parse.User, function(req, res) {
   Parse.Cloud.useMasterKey();
   var user = req.object
 
-  if(user.isNew()) {
-    user.set("admin", !!user.get("admin"))
-    user.set("terms", !!user.get("terms"))
-    user.set("registered", !!user.get("registered"))
-    user.set("displayName", user.get("name"))
-    res.success()
-  } else if(user.dirty("email") && !user.get("displayName")) {
-    var query = new Parse.Query(Testers)
-    var name  = user.get("name")
-    var email = user.get("email")
+  Settings().then(function(settings) {
+    var abTesters = settings.get("abTesters")
 
-    user.set("displayName", name)
-
-    // Check if Beta Tester
-    query.equalTo("email", email)
-    query.first().then(function(tester) {
-      if(tester) {
-        tester.set("name", name)
-        tester.set("user", user)
-        return tester.save()
-      }
-    }).then(function() {
+    if(user.isNew()) {
+      user.set("admin", !!user.get("admin"))
+      user.set("terms", !!user.get("terms"))
+      user.set("registered", !!user.get("registered"))
+      user.set("displayName", user.get("name"))
+      user.set("abTester", abTesters[Math.floor(Math.random() * abTesters.length)])
       res.success()
-    }, function(error) {
-      res.error(error.message)
-    })
-  } else {
-    res.success()
-  }
+    } else if(user.dirty("email") && !user.get("displayName")) {
+      var query = new Parse.Query(Testers)
+      var name  = user.get("name")
+      var email = user.get("email")
+
+      user.set("displayName", name)
+
+      // Check if Beta Tester
+      query.equalTo("email", email)
+      query.first().then(function(tester) {
+        if(tester) {
+          tester.set("name", name)
+          tester.set("user", user)
+          return tester.save()
+        }
+      }).then(function() {
+        res.success()
+      }, function(error) {
+        res.error(error.message)
+      })
+    } else {
+      res.success()
+    }
+  })
 })
