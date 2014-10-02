bbab6e794cae0b4398a1ceee6a6dd121b80e7495
Brian Vallelunga <vallelungabrian@gmail.com>
2014-10-01 21:31:30 -0700

    added  onboarding

--- cloud/beforeSave/users.js
+++ cloud/beforeSave/users.js
@@ -11,6 +11,7 @@ Parse.Cloud.beforeSave(Parse.User, function(req, res) {
     if(user.isNew()) {
       user.set("admin", !!user.get("admin"))
       user.set("terms", !!user.get("terms"))
+      user.set("onboarded", !!user.get("onboarded"))
       user.set("registered", !!user.get("registered"))
       user.set("displayName", user.get("name"))
       user.set("abTester", abTesters[Math.floor(Math.random() * abTesters.length)])
