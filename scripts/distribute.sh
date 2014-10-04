8f6e0c7eb5b8a9d9404ef2c452ef7e133f79fe55
Brian Vallelunga <vallelungabrian@gmail.com>
2014-10-04 15:53:06 -0700

    fixed distribute script

--- scripts/distribute.sh
+++ scripts/distribute.sh
@@ -2,12 +2,13 @@
 
 git add .
 git commit -m "$1"
+git push origin development
 
 git pull
 
 git checkout .
 git checkout master
 git merge origin/development
+git push origin master
 
 git checkout development
-#git push
