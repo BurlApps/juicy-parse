4153dd672c01c3555c3597d75d3bba65732c6b5e
Brian Vallelunga <vallelungabrian@gmail.com>
2014-10-12 15:08:39 -0700

    clear submission form when submitting

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -6,16 +6,18 @@ $(function() {
     e.stopPropagation()
 
     if(enablePosting) {
-      var contact = $(this).find(".contact")
-      var button = $(this).find(".submit").val("sending...")
+      var form = $(this)
+      var contact = form.find(".contact")
+      var button = form.find(".submit").val("sending...")
       enablePosting = false
 
-      $.post($(this).attr("action"), $(this).serialize(), function(response) {
+      $.post(form.attr("action"), form.serialize(), function(response) {
         enablePosting = true
         button.val(response.message)
         button.toggleClass("error", !response.success)
         button.toggleClass("active", response.success)
         contact.toggleClass("active", response.success)
+        form.find("input[type=text], textarea").val("")
       })
     }
   })
