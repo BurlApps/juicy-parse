2ee1b55e22b81d7e0da14ae911549f214e9bde6d
Brian Vallelunga <vallelungabrian@gmail.com>
2014-11-10 16:41:53 -0800

    actually moved to cloudcode

--- cloud/functions/index.js
+++ cloud/functions/index.js
@@ -1,3 +1,4 @@
 require("cloud/functions/facebookFriends")
 require("cloud/functions/shareSms")
 require("cloud/functions/feed")
+require("cloud/functions/topPosts")
---
+++ cloud/functions/topPosts.js
@@ -0,0 +1,18 @@
+var Posts = Parse.Object.extend("Posts")
+
+Parse.Cloud.define("topPosts", function(req, res) {
+  var query = new Parse.Query(Posts)
+  var daysAgo = new Date()
+
+  daysAgo.setDate(daysAgo.getDate() - 2)
+
+  query.limit(req.params.limit)
+  query.equalTo("show", true)
+  query.descending("karma")
+
+  query.find().then(function(posts) {
+    res.success(posts)
+  }, function(error) {
+    res.error(error.description)
+  })
+})
