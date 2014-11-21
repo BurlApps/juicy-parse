0b626066d988d77dbba137bd94a45c4e54ba293b
Brian Vallelunga <vallelungabrian@gmail.com>
2014-11-20 20:27:40 -0800

    Revert "removed user tracking"
    This reverts commit 36a230a394601c2a978218a7a427c69d8eacaf88.

--- cloud/express/routes/feed.js
+++ cloud/express/routes/feed.js
@@ -1,5 +1,6 @@
 var _        = require('underscore')
 var Posts    = Parse.Object.extend("Posts")
+var Settings = require("cloud/util/settings")
 
 module.exports.home = function(req, res) {
   res.render("feed/index", {
@@ -11,40 +12,71 @@ module.exports.posts = function(req, res) {
   var results = []
   var images = req.param("images")
 
-  var query = new Parse.Query(Posts)
-  query.limit(10)
+  Settings().then(function(settings) {
+    var user = new Parse.User()
+    req.settings = settings
 
-  query.equalTo("show", true)
-  query.lessThanOrEqualTo("length", 350)
-  query.descending("createdAt")
+    if(req.session.user) {
+      user.id = req.session.user
+    	return user
+    } else if(req.cookies[req.settings.get("confessionTracker")]) {
+      user.id = req.cookies[req.settings.get("confessionTracker")]
+      return user
+    } else {
+      var random = Math.random().toString(36).slice(2)
+      user.set("username", random)
+      user.set("password", random)
+      user.set("admin", false)
+      user.set("registered", false)
+      user.set("terms", false)
+      return user.signUp()
+    }
+  }).then(function(user) {
+    res.cookie(req.settings.get("confessionTracker"), user.id, {
+	    maxAge: 9000000000,
+	    httpOnly: true
+	  })
 
-  if(images == "true") {
-    query.exists("image")
-  } else if(images == "false") {
-    query.doesNotExist("image")
-  }
+    var query = new Parse.Query(Posts)
+    query.limit(10)
 
-  query.find().then(function(posts) {
-    var promise = Parse.Promise.as()
+    query.equalTo("show", true)
+    query.lessThanOrEqualTo("length", 350)
+    query.notEqualTo("likedUsers", user)
+    query.descending("createdAt")
 
-		_.each(posts, function(post) {
-      promise = promise.then(function() {
-        var image = post.get("image")
+    if(images == "true") {
+      query.exists("image")
+    } else if(images == "false") {
+      query.doesNotExist("image")
+    }
 
-        results.push({
-          message: post.get("flatContent"),
-          image: (image) ? image.url() : null,
-          background: post.get("background") || [],
-          alpha: post.get("darkenerAlpha")
+    query.find().then(function(posts) {
+      var promise = Parse.Promise.as()
+
+  		_.each(posts, function(post) {
+        promise = promise.then(function() {
+          var likedRelation = post.relation("likedUsers")
+          var image = post.get("image")
+
+          likedRelation.add(user)
+          post.save()
+
+          results.push({
+            message: post.get("flatContent"),
+            image: (image) ? image.url() : null,
+            background: post.get("background") || [],
+            alpha: post.get("darkenerAlpha")
+          })
         })
       })
-    })
 
-    return promise
-  }).then(function() {
-    res.json(results)
-  }, function(error) {
-    console.log(error)
-    res.json([])
+      return promise
+    }).then(function() {
+      res.json(results)
+    }, function(error) {
+      console.log(error)
+      res.json([])
+    })
   })
 }
